4646c00b07d8988a0f7299bf5024da5282b84b44
Nino <nino@ninux.org>
2019-10-13 15:47:39 +0200

    otaa+abp working example

--- examples/send_recv/main/main.cpp
+++ examples/send_recv/main/main.cpp
@@ -15,6 +15,10 @@
 #include "nvs_flash.h"
 
 #include "TheThingsNetwork.h"
+#include "../src/lmic/oslmic.h"
+#include "../src/lmic/lmic.h"
+#include "../src/hal/hal_esp32.h"
+
 
 // NOTE:
 // The LoRaWAN frequency and the radio chip must be configured by running 'make menuconfig'.
@@ -22,11 +26,11 @@
 
 // Copy the below hex string from the "Device EUI" field
 // on your device's overview page in the TTN console.
-const char *devEui = "????????????????";
+const char *devEui = "0000000000000015";
 
 // Copy the below two lines from bottom of the same page
-const char *appEui = "????????????????";
-const char *appKey = "????????????????????????????????";
+const char *appEui = "0000000000000005";
+const char *appKey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 // Pins and other resources
 #define TTN_SPI_HOST      HSPI_HOST
@@ -45,7 +49,67 @@ static TheThingsNetwork ttn;
 const unsigned TX_INTERVAL = 30;
 static uint8_t msgData[] = "Hello, world";
 
+static RTC_DATA_ATTR struct timeval sleep_enter_time;
+RTC_DATA_ATTR u4_t RTCnetid;
+RTC_DATA_ATTR u4_t RTCdevaddr;
+RTC_DATA_ATTR u1_t RTCnwkKey[16];
+RTC_DATA_ATTR u1_t RTCartKey[16];
+RTC_DATA_ATTR int RTCseqnoUp;
+RTC_DATA_ATTR int RTCseqnoDn;
+RTC_DATA_ATTR int deepsleep=0;
 
+void sleeppa(int sec)
+{
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    int sleep_time_ms = (now.tv_sec - sleep_enter_time.tv_sec) * 1000 + (now.tv_usec - sleep_enter_time.tv_usec) / 1000;
+
+    switch (esp_sleep_get_wakeup_cause()) {
+        case ESP_SLEEP_WAKEUP_EXT1: {
+            uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
+            if (wakeup_pin_mask != 0) {
+                int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
+                printf("Wake up from GPIO %d\n", pin);
+            } else {
+                printf("Wake up from GPIO\n");
+            }
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_TIMER: {
+            printf("Wake up from timer. Time spent in deep sleep: %dms\n", sleep_time_ms);
+	    //deepsleep=1;
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_UNDEFINED:
+        default:
+	    //deepsleep=0;
+            printf("Not a deep sleep reset\n");
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    const int wakeup_time_sec = sec;
+    printf("Enabling timer wakeup, %ds\n", wakeup_time_sec);
+    esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
+
+    const int ext_wakeup_pin_1 = 25;
+    const uint64_t ext_wakeup_pin_1_mask = 1ULL << ext_wakeup_pin_1;
+    const int ext_wakeup_pin_2 = 26;
+    const uint64_t ext_wakeup_pin_2_mask = 1ULL << ext_wakeup_pin_2;
+
+    printf("Enabling EXT1 wakeup on pins GPIO%d, GPIO%d\n", ext_wakeup_pin_1, ext_wakeup_pin_2);
+    esp_sleep_enable_ext1_wakeup(ext_wakeup_pin_1_mask | ext_wakeup_pin_2_mask, ESP_EXT1_WAKEUP_ANY_HIGH);
+
+    // Isolate GPIO12 pin from external circuits. This is needed for modules
+    // which have an external pull-up resistor on GPIO12 (such as ESP32-WROVER)
+    // to minimize current consumption.
+    //rtc_gpio_isolate(GPIO_NUM_12);
+
+    printf("Entering deep sleep\n");
+    gettimeofday(&sleep_enter_time, NULL);
+    deepsleep=1;
+    esp_deep_sleep_start();
+}
 void sendMessages(void* pvParameter)
 {
     while (1) {
@@ -53,7 +117,10 @@ void sendMessages(void* pvParameter)
         TTNResponseCode res = ttn.transmitMessage(msgData, sizeof(msgData) - 1);
         printf(res == kTTNSuccessfulTransmission ? "Message sent.\n" : "Transmission failed.\n");
 
-        vTaskDelay(TX_INTERVAL * 1000 / portTICK_PERIOD_MS);
+	RTCseqnoUp=LMIC.seqnoUp;	
+	RTCseqnoDn=LMIC.seqnoDn;	
+	sleeppa(20);
+        //vTaskDelay(TX_INTERVAL * 1000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -89,20 +156,46 @@ extern "C" void app_main(void)
 
     // Configure the SX127x pins
     ttn.configurePins(TTN_SPI_HOST, TTN_PIN_NSS, TTN_PIN_RXTX, TTN_PIN_RST, TTN_PIN_DIO0, TTN_PIN_DIO1);
-
-    // The below line can be commented after the first run as the data is saved in NVS
-    ttn.provision(devEui, appEui, appKey);
-
     ttn.onMessage(messageReceived);
 
-    printf("Joining...\n");
-    if (ttn.join())
-    {
-        printf("Joined.\n");
-        xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void* )0, 3, nullptr);
-    }
-    else
-    {
-        printf("Join failed. Goodbye\n");
+    // The below line can be commented after the first run as the data is saved in NVS
+    if(!deepsleep){
+    	ttn.provision(devEui, appEui, appKey);
+
+
+    	printf("Joining...\n");
+    	if (ttn.join())
+    	{
+    	    printf("Joined.\n");
+	    LMIC_getSessionKeys (&RTCnetid, &RTCdevaddr,RTCnwkKey,RTCartKey);	    
+            printf("netid:%x\n",RTCnetid);
+            printf("devaddr:%x\n",RTCdevaddr);
+            int i=0;
+            int k=0;
+            for(i=0;i<16;i++){
+            	printf("%.2x",RTCnwkKey[i]);
+            }
+            printf("\n");
+            for(k=0;k<16;k++){
+            	printf("%.2x",RTCartKey[k]);
+            }
+            printf("\n");
+    	    xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void* )0, 3, nullptr);
+    	}
+    	else
+    	{
+    	    printf("Join failed. Goodbye\n");
+    	    sleeppa(600);
+    	}
+    }else{
+    	LMIC_reset();
+    	//hal_enterCriticalSection();
+    	LMIC_setSession (RTCnetid, RTCdevaddr, RTCnwkKey, RTCartKey);
+    	//hal_leaveCriticalSection();
+	LMIC.seqnoUp=RTCseqnoUp;
+	LMIC.seqnoDn=RTCseqnoDn;
+	printf("mando il messaggio in ABP con numeri di sequenza Up:%d Dn:%d\n",LMIC.seqnoUp,LMIC.seqnoDn);
+    	xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void* )0, 3, NULL);
+
     }
 }
